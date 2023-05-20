f1dccfaba577402bec7af961aa5e064540ccd2fe
Andy Albrecht <aalbrecht33733@gmail.com>
2023-05-20 16:31:28 -0400

    Another safety push

--- client/main.js
+++ client/main.js
@@ -1,15 +1,82 @@
-// Create *
-const submitButton = document.querySelector(".btn btn-primary");
-const loginBtn = document.getElementById("loginBtn");
-
+const submitButton = document.querySelector(".btn.btn-primary");
+// const loginBtn = document.getElementById("loginBtn");
+const form = document.getElementById('lostFoundForm');
 const fortuneBtn = document.querySelector("#fortuneButton");
 const dadJokeButton = document.querySelector('#dadJokeButton');
 const jokes = document.querySelector('#jokes');
+const postForm = document.getElementById('postForm');
+const commentsList = document.getElementById('comments');
 
 
+// Handle form submission...
+function handleFormSubmission(event) {
+    event.preventDefault();
+    const form = document.getElementById('lostFoundForm');
+    const formData = new FormData(form);
+    const method = form.getAttribute('method');
+    const url = form.getAttribute('action');
+
+    const data = {};
+    for (let [key, value] of formData) {
+        data[key] = value;
+    }
+
+    // Example using axios.get
+    axios.get('https://reqres.in/api/users/2')
+        .then(response => {
+            console.log('GET request succeeded:', response.data);
+            form.reset();
+    })
+        .catch(error => {
+            console.error('GET request failed:', error);
+    });
+
+    // Example using axios.put
+    const updatedUserData = {
+        name: 'John Doe',
+        job: 'Software Engineer'
+    };
+
+    // PUT
+    axios.put('https://reqres.in/api/users/2', updatedUserData)
+        .then(response => {
+            console.log('PUT request succeeded:', response.data);
+            form.reset();
+    })
+        .catch(error => {
+            console.error('PUT request failed:', error);
+    });
+    // Update User intp
+    const newUserData = {
+        name: 'Jonathan Doe-Ray',
+        job: 'Software Architect'
+    };
+
+    // POST  newUserData*
+    axios.post('https://reqres.in/api/users/2', newUserData)
+        .then(response => {
+        console.log('POST request succeeded:', response.data);
+    // Clear the input fields
+        form.reset();
+    })
+    .catch(error => {
+        console.error('POST request failed:', error);
+    });
+
+    // DELETE
+    axios.delete('https://reqres.in/api/users/2')
+        .then(response => {
+            console.log('DELETE request succeeded:', response.data);
+            form.reset();
+    })
+        .catch(error => {
+            console.error('DELETE request failed:', error);
+    });
+
+};
 
 
-// 
+// This is being exported from controller.js
 const getFortune = () => {
     axios.get("http://localhost:4000/api/fortune/")
         .then(res => {
@@ -19,7 +86,7 @@ const getFortune = () => {
     });
 }
 
-//
+// axios.get from a Dad Joke Json file*
 const getDadJoke = async () => {
     try {
         const config = { headers: { Accept: 'application/json' }}
@@ -41,7 +108,28 @@ const addNewJoke = async () => {
 }
 
 
-// complimentBtn.addEventListener('click', getCompliment);
+postForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission and page reload
+
+    // Retrieve the values from the input fields
+    const usernameInput = postForm.elements.username;
+    const commentsInput = postForm.elements.comments;
+    const username = usernameInput.value;
+    const comments = commentsInput.value;
+
+    // Create a new <li> element
+    const newComment = document.createElement('li');
+    newComment.textContent = `${username}: ${comments}`;
+
+    // Append the new <li> to the <ul>
+    commentsList.appendChild(newComment);
+
+    // Clear the input fields
+    usernameInput.value = '';
+    commentsInput.value = '';
+});
+
+form.addEventListener('submit', handleFormSubmission);
 fortuneBtn.addEventListener('click', getFortune);
 dadJokeButton.addEventListener('click', addNewJoke);
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
@@ -7,17 +7,40 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { getCompliment } = require('./controller')
 const { getFortune } = require('./controller')
 
+// Endpoint for handling form submissions
+app.post('/submit', (req, res) => {
+  const { name, description, contact } = req.body;
+  // Handle form submission logic here
+
+  res.sendStatus(200);
+});
+
+// Endpoint for handling PUT requests
+app.put('/items/:id', (req, res) => {
+  const itemId = req.params.id;
+  const { name, description, contact } = req.body;
+  // Handle PUT request logic here
+});
+
+// Endpoint for handling DELETE requests
+app.delete('/items/:id', (req, res) => {
+  const itemId = req.params.id;
+  // Handle DELETE request logic here
+});
+
+
+
 // Form submision
-// app.post(`/api/movies`, createMovie);
-// app.put(`/api/movies/:id`, updateMovie);
-// app.delete(`/api/movies/:id`, deleteMovie);
+app.post(`/api/users/2`, handleFormSubmission);
+app.post(`/api/users/2`, handleFormSubmission);
+app.put(`/api/users/2`, handleFormSubmission);
+app.delete(`/api/users/2`, handleFormSubmission);
 
 // .get from controller.js
 app.get("/api/fortune", getFortune);
 
 
 
-app.listen(4000, () => console.log("===> Server running on 4000 <==="));
+app.listen(4000, () => console.log("===> Server Running on 4000 <==="));
